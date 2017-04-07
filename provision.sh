81ebe82f1c66487a4a5b5de28405604695662f5a
gomesuit <takashi.inoue@theport.jp>
2017-04-07 18:30:50 +0900

    リファクタリング

--- provision.sh
+++ provision.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# install dependency package
+yum install -y gcc-c++ pcre-devel zlib-devel make unzip openssl-devel perl-devel perl-ExtUtils-Embed gd-devel
+
 # install nps
-yum install -y gcc-c++ pcre-devel zlib-devel make unzip
 NPS_VERSION=1.11.33.4
 cd
 wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.zip
@@ -18,11 +20,47 @@ cd
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}/
-./configure --add-module=$HOME/ngx_pagespeed-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS}
+./configure --add-module=$HOME/ngx_pagespeed-${NPS_VERSION}-beta \
+            --prefix=/etc/nginx \
+            --sbin-path=/usr/sbin/nginx \
+            --conf-path=/etc/nginx/nginx.conf \
+            --http-client-body-temp-path=/var/lib/nginx/tmp/client_body \
+            --http-proxy-temp-path=/var/lib/nginx/tmp/proxy \
+            --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi \
+            --pid-path=/var/run/nginx.pid \
+            --lock-path=/var/run/nginx.lock \
+            --error-log-path=/var/log/nginx/error.log \
+            --http-log-path=/var/log/nginx/access.log \
+            --with-http_gzip_static_module \
+            --with-http_stub_status_module \
+            --with-http_ssl_module \
+            --with-pcre \
+            --with-file-aio \
+            --with-http_realip_module \
+            --with-http_perl_module \
+            --with-http_image_filter_module \
+            --without-http_scgi_module \
+            --without-http_uwsgi_module
 make
 make install
 
+groupadd --gid 911 nginx
+useradd --gid nginx \
+        --uid 995 \
+        --no-create-home \
+        --home-dir /var/lib/nginx \
+        --shell /sbin/nologin \
+        --system \
+        --comment "Nginx web server" nginx
+
+mkdir -p /var/lib/nginx/tmp/client_body
+mkdir -p /var/lib/nginx/tmp/proxy
+chown -R nginx.nginx /var/lib/nginx
+
+# logrotate
+\cp -f /vagrant/settings/logrotate.nginx /etc/logrotate.d/nginx
+
 # setting nginx.service
-cp /vagrant/settings/nginx.service /usr/lib/systemd/system/nginx.service
+\cp -f /vagrant/settings/nginx.service /usr/lib/systemd/system/nginx.service
 systemctl daemon-reload
 
