5d3741f69dffbedaa37946a2cae9c43867f6ea4f
gomesuit <takashi.inoue@theport.jp>
2017-04-07 20:02:03 +0900

    memcachedとの連携

--- provision.sh
+++ provision.sh
@@ -82,6 +82,7 @@ systemctl start docker
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # run memcached
 \cp -r /vagrant/settings/docker-compose.yml ~/
